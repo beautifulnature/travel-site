29adf54c955fc5d351986751dfcd11030c8a035c
beautifulnature <36604320+beautifulnature@users.noreply.github.com>
2022-03-10 10:54:40 +0530

    completed lazy loading images

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,5 @@
 import '../styles/styles.css'
+import 'lazysizes'
 // import Person from './modules/Person';
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
