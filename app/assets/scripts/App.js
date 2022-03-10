11c5933d2ce4f48ff3b3fa40d81defca8bd7adc4
beautifulnature <36604320+beautifulnature@users.noreply.github.com>
2022-03-10 10:36:27 +0530

    completed modal overlay

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,30 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+// import Modal from './modules/Modal'
 
-let stickyHeader = new StickyHeader();
+new StickyHeader();
 
-let mobileMenu = new MobileMenu();
+new MobileMenu();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+let modal;
+
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if (typeof modal == 'undefined') {
+            import(/* webpackChunckName: "modal" */ './modules/Modal').then(x => {
+                modal = new x.default();
+                setTimeout(() => modal.openTheModal(), 20);
+            }).catch(() => console.log('there was a problem.'));
+        } else {
+            modal.openTheModal();
+        }
+    })
+})
+
+// new Modal();
 
 // alert('The sky is blue');
 if (module.hot) {
